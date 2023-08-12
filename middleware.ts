4ae6e3e1f3f3d897ef27cce76dabd75a17a566d2
Achalogy <achalogy@gmail.com>
2023-08-12 13:42:49 -0500

    Verify JWT Token

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,21 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { verify } from "jsonwebtoken";
+import { jwtVerify } from 'jose';
 
-export function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest) {
   // if (request.nextUrl.pathname == "/") return
-  const tokenCookie = req.cookies.get("token");
-  if(!tokenCookie && req.nextUrl.pathname != "/admin/login") {
+
+  const secret = new TextEncoder().encode(process.env.TOKEN_KEY)
+
+  const tokenCookie = req.cookies.get("token")?.value;
+  let tokenIsValid: any = false;
+  try {
+    tokenIsValid = await jwtVerify(tokenCookie ?? "", secret)
+  } catch {
+
+  }
+  if (!tokenIsValid && req.nextUrl.pathname != "/admin/login") {
     return NextResponse.redirect(new URL('/', req.url))
   }
 }
